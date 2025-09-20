9c0f9062d50812ae327b2896a3bc5a6d79332843
davidbera1 <dataaberadze@gmail.com>
2025-09-20 21:17:02 +0400

    added helper classes for networking

---
+++ app/src/main/java/com/db/cryptotracker/core/data/networking/ConstructUrl.kt
@@ -0,0 +1,11 @@
+package com.db.cryptotracker.core.data.networking
+
+import com.db.cryptotracker.BuildConfig
+
+fun constructUrl(url: String): String {
+    return when {
+        url.contains(BuildConfig.BASE_URL) -> url
+        url.startsWith("/") -> BuildConfig.BASE_URL + url.drop(1)
+        else -> BuildConfig.BASE_URL + url
+    }
+}
